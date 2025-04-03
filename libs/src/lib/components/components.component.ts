f3ac3abf8b8d2f2ea98cc4b701b1c9c7a5b19c00
tri.tdm <tritruong121099@gmail.com>
2025-04-03 23:06:13 +0700

    init lib

---
+++ libs/src/lib/components/components.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ngx-e-com-components',
+  imports: [CommonModule],
+  template: `<p>Components works!</p>`,
+  styles: ``,
+})
+export class ComponentsComponent {}
